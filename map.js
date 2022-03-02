484f374f660cf1ca4c752be502a974dc1636ee6b
laís sousa <cavalcantislais@gmail.com>
2022-03-02 00:32:51 -0300

    first commit

---
+++ map.js
@@ -0,0 +1,19 @@
+var map = L.map("map", {
+  center: [52.51281891416161, 13.38923196153867],
+  zoom: 17,
+ 
+  minZoom: 15,
+});
+
+var marker = L.marker([52.51281891418662, 13.389221317200835]).addTo(map);
+var circle = L.circle([52.51281891418662, 13.389221317200835], {
+    color: '#f6d86a',
+    fillColor: 'rgb(246, 216, 106, 0.4)',
+    fillOpacity: 0.5,
+    radius: 50
+}).addTo(map);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
